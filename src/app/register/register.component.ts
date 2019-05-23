57e12ee72f6da25c937fbe582d6cc4073c9b7c84
sergiyZinovyev <thrion1@gmail.com>
2019-05-23 11:05:19 +0300

    next form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  warEmail: string = 'hidd';
+  warPass: string = 'hidd';
+ 
+
+  registerForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    fullname: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('', Validators.required),
+  });
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    if(this.email.valid){
+      this.warEmail = "hidd";
+    }
+    else{this.warEmail = "vis";}
+
+    if(this.password.valid){
+      this.warPass = "hidd";
+    }
+    else{this.warPass = "vis";}
+
+    if(this.registerForm.valid){console.log(this.registerForm.value);}
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+  get password() {
+    return this.registerForm.get('password');
+  }
+ 
 }
